2e20fa4282095cd9272f4eb5dd02312976e8efbe
DeeMMoon <37477667+DeeMMoon@users.noreply.github.com>
2022-11-11 11:51:32 +0300

    Delete Day06 directory

--- Day06/ex02/Tests/target/test-classes/data.sql
+++
@@ -1,7 +0,0 @@
-INSERT INTO shop.product(name, price) VALUES
-('product1', 100),
-('product2', 110),
-('product3', 120),
-('product4', 130),
-('product5', 140),
-('product6', 150);
--- Day06/ex03/Tests/src/main/java/edu.school21/exceptions/EntityNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package edu.school21.exceptions;
-
-public class EntityNotFoundException extends Exception{
-    public EntityNotFoundException() {
-        super("Error: no user is found with the login specified");
-    }
-}
